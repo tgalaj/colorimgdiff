f5ceeb67b4e4d205f71bd68d257113267d510f7c
Shot511 <tomaszgalaj@gmail.com>
2020-06-15 17:34:55 +0200

    Image normalization, calculating rmse and mse metrics, option to choose different colormap

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -15,21 +16,109 @@ struct ImageMetadata
     int nr_channels;
 };
 
-uint8_t* load_image(const std::string & filename, ImageMetadata& img_data)
+tinycolormap::ColormapType setColormapType(const std::string& colormap_name)
 {
-    return stbi_load(filename.c_str(), &img_data.width, &img_data.height, &img_data.nr_channels, 3);
+    if (colormap_name == "Parula")
+    {
+        return tinycolormap::ColormapType::Parula;
+    }
+    else if (colormap_name == "Heat")
+    {
+        return tinycolormap::ColormapType::Heat;
+    }
+    else if (colormap_name == "Hot")
+    {
+        return tinycolormap::ColormapType::Hot;
+    }
+    else if (colormap_name == "Jet")
+    {
+        return tinycolormap::ColormapType::Jet;
+    }
+    else if (colormap_name == "Gray")
+    {
+        return tinycolormap::ColormapType::Gray;
+    }
+    else if (colormap_name == "Magma")
+    {
+        return tinycolormap::ColormapType::Magma;
+    }
+    else if (colormap_name == "Inferno")
+    {
+        return tinycolormap::ColormapType::Inferno;
+    }
+    else if (colormap_name == "Plasma")
+    {
+        return tinycolormap::ColormapType::Plasma;
+    }
+    else if (colormap_name == "Viridis")
+    {
+        return tinycolormap::ColormapType::Viridis;
+    }
+    else if (colormap_name == "Cividis")
+    {
+        return tinycolormap::ColormapType::Cividis;
+    }
+    else if (colormap_name == "Github")
+    {
+        return tinycolormap::ColormapType::Github;
+    }
+}
+
+std::vector<uint8_t> load_image(const std::string & filename, ImageMetadata& img_data)
+{
+    auto* data = stbi_load(filename.c_str(), &img_data.width, &img_data.height, &img_data.nr_channels, 3);
+
+    std::vector<uint8_t> img(data, data + img_data.width * img_data.height * 3);
+
+    stbi_image_free(data);
+
+    return img;
+}
+
+std::vector<double> luma(const std::vector<uint8_t>& img)
+{
+    const unsigned num_pixels = img.size() / 3;
+
+    std::vector<double> luma(num_pixels);
+
+    for (unsigned i = 0; i < num_pixels; ++i)
+    {
+        /* Conver to [0, 1] range */
+        double r = img[3 * i + 0] / 255.0;
+        double g = img[3 * i + 1] / 255.0;
+        double b = img[3 * i + 2] / 255.0;
+
+        /* Calculate luminance */
+        luma[i] = r * 0.2126 + g * 0.7152 + b * 0.0722;
+    }
+
+    return luma;
+}
+
+/* Performs linear normalization: https://en.wikipedia.org/wiki/Normalization_(image_processing) */
+void normalize_image_linear(std::vector<double> & img, double new_min, double new_max)
+{
+    const auto [min, max] = std::minmax_element(std::begin(img), std::end(img));
+    const double ratio = (new_max - new_min) / (*max - *min);
+
+    for (auto& luma : img)
+    {
+        luma = (luma - *min) * ratio + new_min;
+    }
 }
 
 int main(int argc, char* argv[])
 {
-    // TODO:
-    // - Add cmd arg option to choose a desired colormap
-
     cxxopts::Options options("colorimgdiff", "Creates diff image of ref(erence) and src (source) images. It simply computes luma difference between ref and src.\n");
-    options.add_options()("r,ref", "Relative path to reference image WITH extension [REQUIRED]",             cxxopts::value<std::string>())
-                         ("s,src", "Relative path to source image WITH extension    [REQUIRED]",             cxxopts::value<std::string>())
-                         ("o,out", "Relative path to output image WITHOUT extension (it'll be a PNG image)", cxxopts::value<std::string>()->default_value("output_diff"))
-                         ("v,verbose", "Verbose output",                                                     cxxopts::value<bool>()->default_value("false"))
+    options.add_options()("r,ref", "Relative path to reference image WITH extension [REQUIRED]",                 cxxopts::value<std::string>())
+                         ("s,src", "Relative path to source image WITH extension    [REQUIRED]",                 cxxopts::value<std::string>())
+                         ("o,out", "Relative path to output image WITHOUT extension (it'll be a PNG image)",     cxxopts::value<std::string>()->default_value("output_diff"))
+                         ("c,colormap", "Changes the default colormap. Possible options are: Parula, Heat, "
+                                        "Hot, Jet, Gray, Magma, Inferno, Plasma, Viridis, Cividis, Github.",     cxxopts::value<std::string>()->default_value("Hot"))
+                         //("i,interpolate", "Choose a value from range [1, 255] if you want to disable color "
+                         //                  "interpolation (default) and want to assign several values to the "
+                         //                  "same color.",                                                        cxxopts::value<int>()->default_value("-1"))
+                         ("v,verbose", "Verbose output",                                                         cxxopts::value<bool>()->default_value("false"))
                          ("h,help", "Prints this message");
 
     auto cmd_result = options.parse(argc, argv);
@@ -46,7 +135,10 @@ int main(int argc, char* argv[])
         exit(0);
     }
 
-    bool verbose_output = cmd_result["verbose"].as<bool>();
+    int interpolation_ranges = -1;// cmd_result["interpolate"].as<int>();
+    bool verbose_output      = cmd_result["verbose"].as<bool>();
+    auto colormap_type       = setColormapType(cmd_result["colormap"].as<std::string>());
+   
 
     std::string ref_filename = cmd_result["ref"].as<std::string>();
     std::string src_filename = cmd_result["src"].as<std::string>();
@@ -59,58 +151,68 @@ int main(int argc, char* argv[])
 
     if (verbose_output)
     {
-        if (!ref_data)
+        if (ref_data.empty())
         {
             std::cerr << "Couldn't load " << ref_filename << std::endl;
             return 1;
         }
 
-        if (!src_data)
+        if (src_data.empty())
         {
             std::cerr << "Couldn't load " << src_filename << std::endl;
             return 1;
         }
 
-        if (ref_metadata.width != src_metadata.width || ref_metadata.height != src_metadata.height)
+        if (ref_data.size() != src_data.size())
         {
             std::cerr << "Ref ans Src images' dimensions don't match!" << std::endl;
             return 1;
         }
     }
 
-    std::vector<uint8_t> diff_image(ref_metadata.width * ref_metadata.height * 3);
-    for (unsigned i = 0; i < ref_metadata.width * ref_metadata.height; ++i)
+    /* Calculate luminance of both images */
+    auto ref_luma = luma(ref_data);
+    auto src_luma = luma(src_data);
+
+    /* Normalize both images */
+    normalize_image_linear(ref_luma, 0.0, 1.0);
+    normalize_image_linear(src_luma, 0.0, 1.0);
+
+    double mse = 0.0;
+
+    std::vector<uint8_t> diff_image(ref_luma.size() * 3);
+    for (unsigned i = 0; i < ref_luma.size(); ++i)
     {
-        double ref_r = ref_data[3 * i + 0] / 255.0;
-        double ref_g = ref_data[3 * i + 1] / 255.0;
-        double ref_b = ref_data[3 * i + 2] / 255.0;
-
-        double src_r = src_data[3 * i + 0] / 255.0;
-        double src_g = src_data[3 * i + 1] / 255.0;
-        double src_b = src_data[3 * i + 2] / 255.0;
-
-        // Calculate luminance
-        double ref_luma = ref_r * 0.2126 + ref_g * 0.7152 + ref_b * 0.0722;
-        double src_luma = src_r * 0.2126 + src_g * 0.7152 + src_b * 0.0722;;
-
         // Calculate difference
-        double err = std::fabs(ref_luma - src_luma);
+        double err = ref_luma[i] - src_luma[i];
+        mse += err * err;
 
         // Get Color from colormap
-        auto color = tinycolormap::GetColor(err, tinycolormap::ColormapType::Hot);
+        tinycolormap::Color color(1.0, 1.0, 1.0);
+        
+        if (interpolation_ranges < 0)
+        {
+            color = tinycolormap::GetColor(std::fabs(err), colormap_type);
+        }
+        else
+        {
+            // TODO
+        }
+
         diff_image[3 * i + 0] = color.ri();
         diff_image[3 * i + 1] = color.gi();
         diff_image[3 * i + 2] = color.bi();
     }
 
-    stbi_image_free(ref_data);
-    stbi_image_free(src_data);
-
     stbi_write_png(out_filename.c_str(), ref_metadata.width, ref_metadata.height, 3, diff_image.data(), 0);
+
+    mse = mse / ref_luma.size();
 
     if (verbose_output)
     {
-        std::cout << "Saved image " << out_filename << std::endl;
+        std::cout << "Saved image " << out_filename   << std::endl;
+        std::cout << "MSE:  "       << mse            << std::endl;
+        std::cout << "RMSE: "       << std::sqrt(mse) << std::endl;
     }
 
     return 0;
